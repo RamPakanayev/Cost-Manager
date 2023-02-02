e2801caabc8b001f43eec275cd885a3a1fd249b6
RamPakanayev <rampakanayev@gmail.com>
2023-02-02 17:07:39 +0200

    Update addCost.js
    "Added validation functions to ensure the accuracy of cost data before saving to database"

--- routers/addCost.js
+++ routers/addCost.js
@@ -4,48 +4,69 @@ const axios = require('axios');
 const { costDoc } = require('../db/db');
 const url = require('url');
 
+// Function to check if a given day and month are valid
+const isValidDate = (day, month) => {
+return day > 0 && day <= 31 && month > 0 && month <= 12;
+};
+
+// Function to check if a given category is one of the valid categories
+const isValidCategory = (category) => {
+const validCategories = ['food', 'health', 'housing', 'sport', 'education', 'transportation', 'other'];
+return validCategories.includes(category);
+};
+
 // This route is for saving new costs to the database
 router.post('/', async (req, res) => {
-  // Logging message to track that the addCost route is being processed
-  console.log('Processing the new added cost...');
+// Logging message to track that the addCost route is being processed
+console.log('Processing the new added cost...');
 
-  // Getting the parameters passed in the URL
-  const queryObject = url.parse(req.url, true).query;
+// Getting the parameters passed in the URL
+const queryObject = url.parse(req.url, true).query;
 
-  // Destructuring the parameters into separate variables
-  let userId = queryObject.user_id;
-  let year = String(queryObject.year);
-  let month = String(queryObject.month);
-  let day = queryObject.day;
-  let description = queryObject.description;
-  let category = queryObject.category;
-  let sum = queryObject.sum;
-  let id = 0; // Not sure what the purpose of this is
+// Destructuring the parameters into separate variables
+let userId = queryObject.user_id;
+let year = String(queryObject.year);
+let month = String(queryObject.month);
+let day = queryObject.day;
+let description = queryObject.description;
+let category = queryObject.category;
+let sum = queryObject.sum;
+let id = 0; // Not sure what the purpose of this is
 
-  // Creating a new cost document with the given parameters
-  let cost = new costDoc({
-    user_id: userId,
-    year,
-    month,
-    day,
-    id, // Not sure what the purpose of this is
-    description,
-    category,
-    sum,
-  });
+// Check if the date is valid
+if (!isValidDate(day, month)) {
+return res.status(400).send('Invalid date');
+}
 
-  // Trying to save the cost document to the database
-  try {
-    await cost.save();
-    console.log('Cost was saved in the MongoDB dataBase');
-    // cost.printContent(); // Not sure what the purpose of this is
-    id += 1; // Not sure what the purpose of this is
-  } catch (error) {
-    console.error(error);
-  }
+// Check if the category is valid
+if (!isValidCategory(category)) {
+return res.status(400).send('Invalid category');
+}
 
-  // Sending a response indicating that the cost was saved to the database
-  res.send('Cost was saved in the MongoDB dataBase !');
+// Creating a new cost document with the given parameters
+let cost = new costDoc({
+user_id: userId,
+year,
+month,
+day,
+id, // Not sure what the purpose of this is
+description,
+category,
+sum,
+});
+
+// Trying to save the cost document to the database
+try {
+await cost.save();
+console.log('Cost was saved in the MongoDB dataBase');
+// cost.printContent(); // Not sure what the purpose of this is
+id += 1; // Not sure what the purpose of this is
+} catch (error) {
+console.error(error);
+}
+
+// Sending a response indicating that the cost was saved to the database
+res.send('Cost was saved in the MongoDB dataBase !');
 });
 
 module.exports = router;
